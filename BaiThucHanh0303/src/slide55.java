9b1c3ffe70dcadf9047c7544703fae0a4ea5f023
zedvip <nhinhanh904@gmail.com>
2023-03-06 16:51:29 +0700

    add .

---
+++ BaiThucHanh0303/src/slide55.java
@@ -0,0 +1,12 @@
+public class slide55 {
+    public static void main(String[] args) {
+        System.out.println("Cac so chia het cho 5 :");
+        for(int i = 0 ; i <= 200; i++) {
+            if(i % 5 == 0 ) {
+                System.out.println(+i);
+            }
+        }
+    }
+}
+    
+
